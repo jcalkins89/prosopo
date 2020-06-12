3ed4096bd7660b4cffcf7ac5be7186a7e51c8c53
Josh Calkins <jcalkins89@gmail.com>
2020-06-12 16:01:16 -0400

    Setup initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <Container>
+      {/* <Navigation /> */}
+      <h1>Hello World</h1>
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  text-align: center;
+`;
