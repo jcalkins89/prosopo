4c7b97a03febbb4384bd069013935ad4a7a3c320
Josh Calkins <jcalkins89@gmail.com>
2020-06-19 18:15:44 -0400

    Added Facial Recognition component

--- src/components/face-recognition/FaceRecognition.tsx
+++ src/components/face-recognition/FaceRecognition.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-const FaceRecognition = ({ imageUrl }: { imageUrl: string }) => {
+const FaceRecognition = ({
+  imageUrl,
+  box,
+}: {
+  imageUrl: string;
+  box: Object;
+}) => {
   return (
     <ImageContainer>
       <InnerContainer>
-        <Image alt="" src={imageUrl} />
+        <Image alt="" id="inputimage" src={imageUrl} />
+        <div></div>
       </InnerContainer>
     </ImageContainer>
   );
